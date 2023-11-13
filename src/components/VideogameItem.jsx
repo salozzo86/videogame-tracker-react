f0091e0d47b92a7a7262732c3c80d4b3eb923838
salozzo86 <antonio.malafronte86@gmail.com>
2023-11-13 18:35:29 +0100

    Finalized logic for each VideogameItem + Added platform icons, with rich alt text and title. Added logic to display icons for all platforms coming from databases.

--- src/components/VideogameItem.jsx
+++ src/components/VideogameItem.jsx
@@ -1,13 +1,18 @@
 import Card from './UI/Card';
-import playstation_4 from '../assets/Console Icons SVG/playstation_4.svg';
-import playstation_5 from '../assets/Console Icons SVG/playstation_5.svg';
+import macos from '../assets/Console Icons SVG/macos.svg';
+import nintendo from '../assets/Console Icons SVG/nintendo.svg';
+import xbox from '../assets/Console Icons SVG/xbox.svg';
+import playstation from '../assets/Console Icons SVG/playstation.svg';
+import pc from '../assets/Console Icons SVG/pc.svg';
 
 const platforms_available = {
-  PlayStation: playstation,
   PC: pc,
-  macos: macos,
-  Xbox: xbox_series,
-  Nintendo: nintendo,
+  'PlayStation 5': playstation,
+  'Xbox One': xbox,
+  'PlayStation 4': playstation,
+  'Nintendo Switch': nintendo,
+  'Xbox Series S/X': xbox,
+  macOS: macos,
 };
 
 const VideogameItem = (props) => {
@@ -19,7 +24,20 @@ const VideogameItem = (props) => {
         <p className="absolute right-2 top-1 text-sm">{props.status}</p>
         <div className="flex flex-row">
           {props.platforms.map((platform) => {
-            return <img key={platform} src={platforms_available[platform]} />;
+            return (
+              <img
+                key={platform}
+                src={platforms_available[platform]}
+                className="px-1"
+                title={platform}
+                alt={`Main logo for ${platforms_available[platform]
+                  .split('/')
+                  .pop()
+                  .split('.')
+                  .slice(0, -1)
+                  .join('.')}`}
+              />
+            );
           })}
         </div>
       </div>
